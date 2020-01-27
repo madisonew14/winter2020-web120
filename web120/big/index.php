24a7af6e8667ff9c12ab1ff2e24818992284100b
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-01-26 20:08:39 -0800

    Add files via upload

---
+++ web120/big/index.php
@@ -0,0 +1,9 @@
+<? include 'includes/header.php';?>
+
+<!-- 3 images HERE -->
+<h2 class="subheader">BIG</h2>
+
+     Page reserved for BIG
+       
+<!-- PHP calling for footer -->
+<?php include 'includes/footer.php';?>
---
+++ web120/fp/template.php
@@ -0,0 +1,7 @@
+<? include 'includes/header.php';?>
+
+<!-- 3 images HERE -->
+     Page reserved for final project
+       
+<!-- PHP calling for footer -->
+<?php include 'includes/footer.php';?>
