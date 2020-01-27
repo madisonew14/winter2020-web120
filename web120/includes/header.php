c211c1250251eff3438fe8c1ea25ff61c7362ece
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-01-26 20:09:22 -0800

    Add files via upload

---
+++ web120/includes/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Madison Woo: WEB120 Portal Website</title>
+  <meta charset="utf-8" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="stylesheet" href="css/portal.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+ </head>
+ <body>
+     <header>
+     <h1>Madison Woo: WEB120 Portal Website</h1>
+     <nav class="topnav" id="myTopnav">
+       <a href="index.php">Welcome</a>
+         <a href="aia.php">AIA</a>
+         <a href="big/index.php">BIG</a>
+         <a href="flowchart.php">Flowchart</a>
+       <a href="fp/template.php">Final Project</a>
+       <a href="contact.php">Contact Madison</a>
+       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+    </nav>
+     </header>
+     
+   <main class="wrapper">
+       
+<!-- header includes HERE -->
