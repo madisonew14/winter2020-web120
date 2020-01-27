e09c34f723bc98d4115d36611634628fc39fe69c
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-01-26 20:06:56 -0800

    Add files via upload

---
+++ web120/aia.php
@@ -0,0 +1,8 @@
+<? include 'includes/header.php';?>
+
+<!-- 3 images HERE -->
+<h2 class="subheader">AIA</h2>
+     Page reserved for AIA research
+       
+<!-- PHP calling for footer -->
+<?php include 'includes/footer.php';?>
---
+++ web120/flowchart.php
@@ -0,0 +1,9 @@
+<? include 'includes/header.php';?>
+
+<!-- 3 images HERE -->
+<h2 class="subheader">Flowchart</h2>
+
+     Page reserved for Flowchart
+       
+<!-- PHP calling for footer -->
+<?php include 'includes/footer.php';?>
