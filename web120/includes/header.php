1ceabfdc382eb57e6e01d805060ee08606c6d424
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-01-28 13:00:17 -0800

    Update header.php

--- web120/includes/header.php
+++ web120/includes/header.php
@@ -1,8 +1,10 @@
+<?php include 'portal-config.php'?>
 <!DOCTYPE html>
 <html lang="en">
  <head>
-     <title>Madison Woo: WEB120 Portal Website</title>
+  <title><?=$title?></title>
   <meta charset="utf-8" />
+  <script src="https://use.fontawesome.com/6a71565c22.js"></script>
   <meta name="robots" content="noindex,nofollow" />
   <meta name="viewport" content="width=device-width" />
   <link rel="stylesheet" href="css/portal.css" />
@@ -10,15 +12,10 @@
  </head>
  <body>
      <header>
-     <h1>Madison Woo: WEB120 Portal Website</h1>
+     <h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i> Madison's WEB120 Portal Website</h1>
      <nav class="topnav" id="myTopnav">
-       <a href="index.php">Welcome</a>
-         <a href="aia.php">AIA</a>
-         <a href="big/index.php">BIG</a>
-         <a href="flowchart.php">Flowchart</a>
-       <a href="fp/template.php">Final Project</a>
-       <a href="contact.php">Contact Madison</a>
-       <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+     <?=makeLinks($nav1)?>    
+         <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
     </nav>
      </header>
      
