0d62120d8e3037c1ed6a1e040a8c773e135d9678
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-02-08 16:35:14 -0800

    Update aia.php

--- web120/aia.php
+++ web120/aia.php
@@ -1,8 +1,106 @@
 <? include 'includes/header.php';?>
 
-<!-- 3 images HERE -->
-<h2 class="subheader">AIA</h2>
-     Page reserved for AIA research
+<h2 class="subheader">AIA (Audience, Issues and Approaches)</h2>
+<h3>Client Information</h3>
+<table class="client">
+    <tr>
+        <th>Client's Name</th>
+        <td>Pearl Hsiau</td>
+    </tr>
+    <tr>
+        <th>Business Name</th>
+        <td>Pearlable</td>
+    </tr>
+    <tr>
+        <th>Redesign or brand new?</th>
+        <td>Brand new</td>
+    </tr>
+    <tr>
+        <th>Type of website</th>
+        <td>Artwork portfolio</td>
+    </tr>
+    <tr>
+        <th>Client Goals</th>
+        <td>To present artwork with external links to social media accounts, while being easily found by keywords on search engines. Another goal is to create a channel of communication between the artist and her potential clients.</td>
+    </tr>
+</table>
+
+<br>
+
+<table class="audience">
+    <tr>
+        <th>Audience</th>
+        <th>Issues/Concerns</th>
+        <th>Approach</th>
+    </tr>
+    <tr>
+        <td><strong>Audience #1: Clients</strong><br>
+            Clients age can range anywhere from young adults (18) to as old as 75 or potentially older. They are the biggest audience and the most significant for my client and the website will be built heavily around this audience. Clients are likely to be viewing my clients page on either a desktop viewport, mobile or tablet. My client’s website should be designed for easy use as some clients may not be experienced in navigating websites. Client’s potentially own their own businesses and would hire my client for her artwork. Lastly, a contact form would be an important element in my client’s website. Depending on the client, internet connection could be limited. 
+        </td>
+        <td>
+            <ul>
+                <li>Will most likely want to get in touch with my client</li>
+                <li>Needs an easy way to view my client's artwork to see if she is the right artist for their job</li>
+                <li>Clients will likely want to see my client's social media accounts and will need easy access to these</li>
+                <li>Clients will need an easy way to navigate a way throughout the site between viewing portfolio pages, contact pages and wherever social media account links live</li>
+                <li>Clients may want to know about pricing</li>
+                <li>Since clients may be viewing the website on any viewport they will want to view this website in a responsive way depending on what device they are using</li>
+                <li>Will need to view all works despite low internet connection</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <li>Include a contact form page but on the same page include external links to social media accounts</li>
+                <li>Choose a media slider that will greatly enlarge images when clicked</li>
+                <li>So clients can view past work of my client, we can potentially use a large image slider to showcase the work without slowing down the website and taking up too much room on the page</li>
+                <li>Include an easy to see navigation in the top center of the web page</li>
+                <li>Include media queries to be viewed on any device</li>
+                <li>Resize images in image gallery so they can be loaded no matter what level of internet connection a client may have</li>
+            </ul>
+        </td>
+    </tr>
+     <tr>
+        <td><strong>Audience #2: Employers</strong><br>
+            Potential employers age range between early twenties to late 50s. They could be hiring managers at a company or HR recruiters that will be looking at my client’s website if she considers applying to work for companies. Employers will most likely be viewing the website in their office on desktops or laptop viewports. They will less likely be viewing the website on mobile or tablet devices. Employers will probably be advanced at navigating the internet and have very good internet connection.
+         </td>
+        <td>
+            <ul>
+                <li>Will need an easy way to view my client's work</li>
+                <li>Employers will also want to see a resume or past working experience</li>
+                <li>Clients will likely want to see my client's social media accounts and will need easy access to these</li>
+                <li>Although employers are skilled at using the internet, they review applicants quickly so they will need an easy flow between all the pages to navigate the website easily</li>
+                <li>Employers typically use email as a means of communication with applicants but contact pages could be needed by employers</li>
+            </ul>
+         </td>
+        <td><ul>
+                <li>Add a simple and easy to navigate nav bar at the top to account for employers quickly assessing my client as a job applicant</li>
+                <li>Potentially add a page to my client's resume</li>
+                <li>Include a contact page that includes my client's email and cell phone number for professional means of contact regarding jobs</li>
+                <li>Choose a media slider that will greatly enlarge images when clicked. An image slider must be carefully choosen to impress employers. It must look professional and on trend with current image sliders (the image slider is the greatest focus for employers)</li>
+            </ul>
+         </td>
+    </tr>
+    <tr>
+        <td><strong>Audience #3: Other designers</strong><br>
+            Fellow designers can also have a large range between young adults to those over 50. They are most likely the most skilled in using the internet, but could be viewing the website on any viewport. They could be visiting my clients website and would want to network and/or connect through my clients social media accounts. Most designers will have access to strong internet connection. 
+         </td>
+        <td>
+            <ul>
+                <li>Other designers will want to have access to my client's social media accounts like Instagram and/or Dribbble. Connecting through social media can be more appealing rather than connecting through contact form</li>
+                <li>Since other designers will potentially want to network, a contact page will be needed (or links to other means of contant)</li>
+                <li>Fellow designers will want an easy way to view my client's portfolio work</li>
+            </ul>
+        </td>
+        <td>
+            <ul>
+                <li>Potentially include an embeded Instagram element to the website since social media is very important to artists and designers</li>
+                <li>Include clear and visible external links to my client's social media accounts (potentially in a fixed position on the home page or every page. Not just on the contact page).</li>
+                <li>Must include media queries to account for other designers viewing my client's website on a variety of devices</li>
+            </ul>
+        </td>
+    </tr>
+
+</table>
        
 <!-- PHP calling for footer -->
 <?php include 'includes/footer.php';?>
