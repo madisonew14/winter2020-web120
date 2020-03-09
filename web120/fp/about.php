bfe7c713a42c9af83a1e969264fb55036c787530
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-03-08 20:12:07 -0700

    Update about.php

--- web120/fp/about.php
+++ web120/fp/about.php
@@ -1,8 +1,14 @@
-<?php include 'header.php'?>
+<?php include 'includes/header.php'?>
 
 <div id="content">
-<img src="pearl_home.jpg" id="homepageimg" alt= "seattle_skyline"/>
+<img src="images/about_photo.jpg" id="aboutimg" alt= "seattle_skyline"/>
 
 <h2><?=$h2?></h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br />
+    
+<a href="files/PeiWenHsiauresume.pdf" alt="photo of Pearl" target="_blank" class="button" style="vertical-align:middle"><span>Resume </span></a>
+
+<a href="files/PeiWenHsiauresume.pdf" alt="Pearl's downloadable resume" target="_blank" class="button" style="vertical-align:middle"><span>Instagram </span></a>
 </div>
-<?php include 'footer.php'?>
+<?php include 'includes/footer.php'?>
