9bec611e9085ed01f1eb756d15d4fec60947dc3b
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-03-08 20:08:23 -0700

    Update header.php

--- web120/fp/includes/header.php
+++ web120/fp/includes/header.php
@@ -1,30 +1,38 @@
-<?php include 'fp-config.php'?>
-<!DOCTYPE html>
+<?php include 'includes/fp-config.php'?>
+<!doctype html>
 <html lang="en">
- <head>
-  <title>Madison's WEB120 Portal Page</title>
-  <meta charset="utf-8" />
-  <script src="https://use.fontawesome.com/6a71565c22.js"></script>
-  <meta name="robots" content="noindex,nofollow" />
-  <meta name="viewport" content="width=device-width" />
-  <link rel="stylesheet" href="css/fp.css" />
-  <link rel="stylesheet" href="css/nav.css" />
- </head>
- <body>
-     <header>
-     <h1><i class="logo fa fa-home"></i> Madison's WEB120 Portal Website</h1>
-     <nav class="topnav" id="myTopnav">
-     <a class="active" href="index.php">Welcome</a>
-<a href="big/index.php">Big</a>
-<a href="aia.php">AIA</a>
-<a href="flowchart.php">Flowchart</a>
-<a href="fp/template.php">Final Project</a>
-<a href="contact.php">Contact Madison</a>
+<head>
+<title><?=$title?></title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="css/fp.css" />
     
-         <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
-    </nav>
-     </header>
-     
-   <main class="wrapper">
-       
-<!-- header includes HERE -->
+<!-- HAMBURGER MENU ICON -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<!-- DESKTOP NAV -->
+<nav id="desktopnav">
+  <a href="template.php">Home</a>
+  <a href="about.php">About Pearlable</a>
+  <a href="http://madwoo3.dreamhosters.com/web120/fp/template.php"><img src="images/logo.png" id="logo" alt="Pearl's logo" width="402" height="211"></a>
+  <a href="artwork.php">Artwork</a>
+  <a href="contact.php">Contact</a>
+</nav>
+
+<!-- MOBILE NAV -->
+<div class="topnav" id="myTopnav">
+  <a href="http://madwoo3.dreamhosters.com/web120/fp/template.php"><img src="images/logo.png" id="logo" alt="Pearl's logo" width="402" height="211" class="active"></a>
+  <a href="template.php">Home</a>
+  <a href="about.php">About Pearlable</a>
+  <a href="artwork.php">Artwork</a>
+  <a href="contact.php">Contact</a>
+
+ <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+    </a>
+</div>
