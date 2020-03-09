afd092bab0cbd80fd535bbb195c1dce232b91d32
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-03-08 20:10:26 -0700

    Update artwork.php

--- web120/fp/artwork.php
+++ web120/fp/artwork.php
@@ -1,6 +1,15 @@
-<?php include 'header.php'?>
+<?php include 'includes/header.php'?>
 
-<div id="content">
+<div id="gallery">
 <h2><?=$h2?></h2>
-</div>
-<?php include 'footer.php'?>
+        <a href="concept.php"><img src="images/Concept.jpg" alt="Concept artwork" class="thumbnail"></a>
+
+        <a href="mind_palace.php"><img src="images/Mind_Palace.jpg" alt="Mind Palace artwork" class="thumbnail"></a>
+
+        <a href="courage.php"><img src="images/Courage.jpg" alt="Courage artwork" class="thumbnail"></a>
+
+        <a href="dreamy_rain.php"><img src="images/Dreamy_Rain.jpg" alt="Dreamy Rain artwork" class="thumbnail"></a>
+    
+        <a href="time.php"><img src="images/time.jpg" alt="Time and Relative Dimension artwork" class="thumbnail"></a>
+</div> 
+<?php include 'includes/footer.php'?>
