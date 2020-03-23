51b2b9ba710be7ac44edebce17d91b2cd2c04638
madisonew14 <56178346+madisonew14@users.noreply.github.com>
2020-03-23 13:05:39 -0700

    Update artwork.php

--- web120/fp/artwork.php
+++ web120/fp/artwork.php
@@ -1,15 +1,18 @@
 <?php include 'includes/header.php'?>
 
 <div id="gallery">
-<h2><?=$h2?></h2>
-        <a href="concept.php"><img src="images/Concept.jpg" alt="Concept artwork" class="thumbnail"></a>
-
-        <a href="mind_palace.php"><img src="images/Mind_Palace.jpg" alt="Mind Palace artwork" class="thumbnail"></a>
-
-        <a href="courage.php"><img src="images/Courage.jpg" alt="Courage artwork" class="thumbnail"></a>
-
-        <a href="dreamy_rain.php"><img src="images/Dreamy_Rain.jpg" alt="Dreamy Rain artwork" class="thumbnail"></a>
     
-        <a href="time.php"><img src="images/time.jpg" alt="Time and Relative Dimension artwork" class="thumbnail"></a>
+    <div class="title"><h2><?=$h2?></h2></div>
+    
+    <div class="item1"><a href="concept.php"><img src="images/Concept.jpg" alt="Concept artwork" class="thumbnail"></a></div>
+
+    <div class="item2"><a href="mind_palace.php"><img src="images/Mind_Palace.jpg" alt="Mind Palace artwork" class="thumbnail"></a></div>
+
+    <div class="item3"><a href="courage.php"><img src="images/Courage.jpg" alt="Courage artwork" class="thumbnail"></a></div>
+
+    <div class="item4"><a href="dreamy_rain.php"><img src="images/Dreamy_Rain.jpg" alt="Dreamy Rain artwork" class="thumbnail"></a></div>
+    
+    <div class="item5"><a href="time.php"><img src="images/time.jpg" alt="Time and Relative Dimension artwork" class="thumbnail"></a></div>
+    
 </div> 
 <?php include 'includes/footer.php'?>
